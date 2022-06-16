6208066f9f291a07dfcfc4b3821526ae4fe97ca6
HEI-Franklin <hei.franklin.2@gmail.com>
2022-06-16 23:16:45 +0300

    datatable ne marche pas

--- src/SidenavContent.js
+++ src/SidenavContent.js
@@ -1,132 +1,81 @@
 import Data from './data.json';
 
-
 function SidenavContent(){
     return(
-            <div id="layoutSidenav_content">
-                <main>
-                    <div className="container-fluid px-4">
-                        <h1 className="mt-4">Dashboard</h1>
-                        <ol className="breadcrumb mb-4">
-                            <li className="breadcrumb-item active">Dashboard</li>
-                        </ol>
-                        <div className="row">
-                            <div className="col-xl-3 col-md-6">
-                                <div className="card bg-primary text-white mb-4">
-                                    <div className="card-body">Primary Card</div>
-                                    <div className="card-footer d-flex align-items-center justify-content-between">
-                                        <a className="small text-white stretched-link" href="/#">View Details</a>
-                                        <div className="small text-white"><i className="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-3 col-md-6">
-                                <div className="card bg-warning text-white mb-4">
-                                    <div className="card-body">Warning Card</div>
-                                    <div className="card-footer d-flex align-items-center justify-content-between">
-                                        <a className="small text-white stretched-link" href="/#">View Details</a>
-                                        <div className="small text-white"><i className="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-3 col-md-6">
-                                <div className="card bg-success text-white mb-4">
-                                    <div className="card-body">Success Card</div>
-                                    <div className="card-footer d-flex align-items-center justify-content-between">
-                                        <a className="small text-white stretched-link" href="/#">View Details</a>
-                                        <div className="small text-white"><i className="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-3 col-md-6">
-                                <div className="card bg-danger text-white mb-4">
-                                    <div className="card-body">Danger Card</div>
-                                    <div className="card-footer d-flex align-items-center justify-content-between">
-                                        <a className="small text-white stretched-link" href="/#">View Details</a>
-                                        <div className="small text-white"><i className="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-xl-6">
-                                <div className="card mb-4">
-                                    <div className="card-header">
-                                        <i className="fas fa-chart-area me-1"></i>
-                                        Area Chart Example
-                                    </div>
-                                    <div className="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
-                                </div>
-                            </div>
-                            <div className="col-xl-6">
-                                <div className="card mb-4">
-                                    <div className="card-header">
-                                        <i className="fas fa-chart-bar me-1"></i>
-                                        Bar Chart Example
-                                    </div>
-                                    <div className="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card mb-4">
-                            <div className="card-header">
-                                <i className="fas fa-table me-1"></i>
-                                DataTable Example
-                            </div>
-                            <div className="card-body">
-                                <table id="datatablesSimple">
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </tfoot>
-
-                                    <tbody>
-                                        {Data.map((person) =>{
-                                            return (<tr key={person.id}>
-                                                <td>{person.Name}</td>
-                                                <td>{person.Position}</td>
-                                                <td>{person.Office}</td>
-                                                <td>{person.Age}</td>
-                                                <td>{person.Start_date}</td>
-                                                <td>{person.Salary}</td>
-                                            </tr>);
-                                        })}
-                                    </tbody>
-
-                                </table>
-                            </div>
-                        </div>
+    <div id="layoutSidenav_content">
+        <main>
+            <div className="container-fluid px-4">
+                <h1 className="mt-4">Tables</h1>
+                <ol className="breadcrumb mb-4">
+                    <li className="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                    <li className="breadcrumb-item active">Tables</li>
+                </ol>
+                <div className="card mb-4">
+                    <div className="card-body">
+                        DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the
+                        <a target="_blank" rel="noreferrer" href="https://datatables.net/">official DataTables documentation</a>
+                        .
                     </div>
-                </main>
-                <footer className="py-4 bg-light mt-auto">
-                    <div className="container-fluid px-4">
-                        <div className="d-flex align-items-center justify-content-between small">
-                            <div className="text-muted">Copyright &copy; Your Website 2022</div>
-                            <div>
-                                <a href="/#">Privacy Policy</a>
-                                &middot;
-                                <a href="/#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
+                </div>
+                <div className="card mb-4">
+                    <div className="card-header">
+                        <i className="fas fa-table me-1"></i>
+                        DataTable Example
                     </div>
-                </footer>
+                    <div className="card-body">
+                        <table id="datatablesSimple">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Position</th>
+                                    <th>Office</th>
+                                    <th>Age</th>
+                                    <th>Start date</th>
+                                    <th>Salary</th>
+                                </tr>
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Position</th>
+                                    <th>Office</th>
+                                    <th>Age</th>
+                                    <th>Start date</th>
+                                    <th>Salary</th>
+                                </tr>
+                            </tfoot>
+                            <tbody>
+                                {Data.map((person) => {
+                                    return(
+                                        <tr key={person.id}>
+                                            <td>{person.Name}</td>
+                                            <td>{person.Position}</td>
+                                            <td>{person.Office}</td>
+                                            <td>{person.Age}</td>
+                                            <td>{person.Start_date}</td>
+                                            <td>{person.Salary}</td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
+        </main>
+        <footer className="py-4 bg-light mt-auto">
+            <div className="container-fluid px-4">
+                <div className="d-flex align-items-center justify-content-between small">
+                    <div className="text-muted">Copyright &copy; Your Website 2022</div>
+                    <div>
+                        <a href="/#">Privacy Policy</a>
+                        &middot;
+                        <a href="/#">Terms &amp; Conditions</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
     );
 }
 
