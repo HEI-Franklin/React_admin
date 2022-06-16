ba40d71a0499682b70b2afb8bed522ece50fa3ec
HEI-Franklin <hei.franklin.2@gmail.com>
2022-06-16 21:44:54 +0300

    page index.html

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Nav from "./Nav";
+import Main from "./Main";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import Sidenav from "./SideNav";
+import SidenavContent from "./SidenavContent";
+
+function Main (){
+    return (
+        <div id="layoutSidenav">
+            <Sidenav />
+            <SidenavContent />
+        </div>
+    );
+}
+
+export default Main;
